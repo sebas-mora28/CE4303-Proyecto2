f16eebc3d86d50b73affc597c428319c359f70e7
josfemova <josfemova@gmail.com>
2023-11-15 22:37:22 -0600

    agregar directorio base para biblioteca de control de steppers

---
+++ libplayer/include/player.h
@@ -0,0 +1,9 @@
+#ifndef PLAYER_LIB_H
+#define PLAYER_LIB_H
+
+
+void hello_player(void);
+
+
+
+#endif
---
+++ libplayer/src/player.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <pthread.h>
+#include "player.h"
+
+void hello_player(){
+    printf("hello from the lib");
+}
